c85cb5b69a396e4a9f6a948040a06a0439ef8056
Stephen Wong <4060163+yellowcandle@users.noreply.github.com>
2024-11-28 02:20:31 +0800

    Add Replit configuration with minimal dependencies

--- replit.nix
+++ replit.nix
@@ -1,30 +1,20 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python312
-    pkgs.python312Packages.pip
-    pkgs.python312Packages.setuptools
-    pkgs.python312Packages.wheel
-    pkgs.chromium  # For selenium
-    pkgs.chromedriver  # For selenium
-    pkgs.firefox  # Alternative browser
-    pkgs.geckodriver  # Firefox driver
-    pkgs.xvfb-run  # Virtual framebuffer for headless browser
-    pkgs.which  # Required for webdriver-manager
-    pkgs.gcc  # For compiling some Python packages
-    pkgs.libffi  # Common dependency
-    pkgs.openssl  # For secure connections
-    pkgs.zlib  # Compression library
-  ];
-  env = {
-    PYTHONBIN = "${pkgs.python312}/bin/python3.12";
-    LANG = "en_US.UTF-8";
-    PYTHONIOENCODING = "utf-8";
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libffi
-      pkgs.openssl
-      pkgs.zlib
-    ];
-    CHROME_BIN = "${pkgs.chromium}/bin/chromium";
-    CHROMEDRIVER_PATH = "${pkgs.chromedriver}/bin/chromedriver";
+{
+  description = "Python development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python312
+          python312Packages.pip
+          python312Packages.setuptools
+          python312Packages.wheel
+        ];
+      };
+    });
 }
